a46d6a8189b1b9b0929c8476109d295dd6e71c50
RGiebultowski <raf.giebultowski@gmail.com>
2021-09-21 12:25:05 +0200

    useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+ const [counter, setCounter] = useState(0);
+ const [isActive, setIsActive] = useState(true);
+
+ useEffect(() => {
+   alert('Hello')
+ }, []);
+
+ const handler = () =>{
+   setCounter(prevCounter => prevCounter + 1);
+ }
+
+ const toggleVisibilityCounter = () => setIsActive(prevValue => !prevValue);
+
+ const counterComponent = isActive ? <Counter rerenderCounter={counter} /> : null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggleVisibilityCounter}>Pokaż / ukryj komponent</button>
+      <button onClick={handler}>Przerenderuj Komponent</button>
+      {counterComponent}
     </div>
+  );
+}
+
+const Counter = ({rerenderCounter}) => {
+  const [counter, setCounter] = useState(0);
+
+  const handleMouseMoove = event => setCounter(event.clientX);
+  
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMoove);
+    return () => { //zapobiega wyciakowi danych po odmonowaniu komponentu
+      alert('Komponent jest odmontowywany, następuje odpięcie Listenera!'); 
+      window.removeEventListener('mousemove', handleMouseMoove);
+    }
+  }, [rerenderCounter])
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <p>{rerenderCounter}</p>
+    </div> 
   );
 }
 
